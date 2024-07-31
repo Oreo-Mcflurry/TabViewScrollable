bac8eefeb47b84173a6bbb8431981e2dbcbb9d95
Musk <inhoyoo1029@gmail.com>
2024-07-31 10:46:31 +0900

    Initial Commit

---
+++ TabViewScrollable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TabViewScrollable
+//
+//  Created by 에스지랩 on 7/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabViewScrollable/TabViewScrollableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewScrollableApp.swift
+//  TabViewScrollable
+//
+//  Created by 에스지랩 on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct TabViewScrollableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
